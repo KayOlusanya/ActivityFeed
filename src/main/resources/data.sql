56f7d039a53f57d262e6917f099d913e64a08171
THISISZONE\andrew.bernard <andrew.bernard@zonedigital.com>
2018-02-16 11:43:17 +0000

    added new rows

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into feed
 values(1,'alert type','category','issue status',null,'title','visit engineer','visit time');
-values(2,'alert type','category','issue status',null,'title','visit engineer','visit time');
+insert into feed
+values(2,'INFO','VISIT','issue status','9999-12-31 23:59:59','Your engineer visit is booked','Tuesday 3rd July','8am - 1pm');
+insert into feed
+values(3,'TARIFF','INFO','Save money by switching tariff','9999-12-31 23:59:59','Looking at your recent energy usage we can see switching can save you money','Tuesday 3rd July','8am - 1pm');
+insert into feed
+values(4,'BILL','ACTIONNEEDED','Your energy bill is now ready','9999-12-31 23:59:59','Looking at your recent energy usage we can see switching can save you money','Tuesday 3rd July','8am - 1pm');
